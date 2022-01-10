1d8b265a6d158c3e258e5b4e5fabf969e270345e
saiyfon <97324335+saiyfon@users.noreply.github.com>
2022-01-10 09:51:44 +0700

    Added Exercise 4
    Upload my file

---
+++ Exercise4_Saiyfonh_S.py
@@ -0,0 +1,12 @@
+Found_Eng = 75.5
+Gen_Bus = 88.5
+Intr_Com = 95.2
+Com_Pro = 89.0
+
+print("--- Your Score ---")
+print("Foundation English : ", Found_Eng)
+print("General Business : ", Gen_Bus)
+print("Introduction to Computer System : ", Intr_Com)
+print("Computer Programming : ", Com_Pro)
+
+
