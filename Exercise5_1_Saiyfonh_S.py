7fb4bb8c3f3633584560edb47158f2fd51e0e92d
saiyfon <97324335+saiyfon@users.noreply.github.com>
2022-01-11 14:23:33 +0700

    Update Exercise5_1_Saiyfonh_S.py
    Update

--- Exercise5_1_Saiyfonh_S.py
+++ Exercise5_1_Saiyfonh_S.py
@@ -1,7 +1,7 @@
 #Exercise5_1
 a = int(input("ค่าที 1: "))
 b = int(input("ค่าที 2: "))
-print(a, "+", b, "=", (a+b))
-print(a, "-", b, "=", (a-b))
-print(a, "*", b, "=", (a*b))
-print(a, "/", b, "=", (a/b))
+print(a, "+", b, "=", a+b)
+print(a, "-", b, "=", a-b)
+print(a, "*", b, "=", a*b)
+print(a, "/", b, "=", a/b)
